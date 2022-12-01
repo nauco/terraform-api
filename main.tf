58b2a7bbb85893fbcc7e67a04035b67d7dd5e7d7
hodong <hodong42@gmail.com>
2022-12-01 13:07:36 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,72 +1,90 @@
-### VPC ###
-resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr  ### VPC CIDR은 변수로 받는다. ###
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+resource "aws_vpc" "app_vpc" {
+  cidr_block = var.vpc_cidr
+
   tags = {
-    Name = "vpc-${var.service}"  ### VPC 이름은 Service 명으로 받는다. ###
+    Name = "app-vpc"
   }
 }
 
-### Subnet ###
-resource "aws_subnet" "public-subnet-a" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 4, 3)
-     ### Subnet CIDR은 16개로 나눠진 VPC CIDR Array의 4번째로 받는다. ###
-  availability_zone       = "ap-northeast-2a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "sub-${var.service}-public-a"
-  }
-}
-
-### Internet Gateway ###
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.app_vpc.id
+
   tags = {
-    Name = "igw-${var.service}"
+    Name = "vpc_igw"
   }
 }
-resource "aws_eip" "eip" {
-  vpc               =   true
-  depends_on        =   [aws_internet_gateway.igw]
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.app_vpc.id
+  cidr_block        = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "public-subnet"
+  }
 }
 
-### Route Table ###
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.app_vpc.id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+
   tags = {
-    Name = "rt-${var.service}-public"
-  }
-}
-resource "aws_route_table_association" "publicrtasso-a" {
-  subnet_id = aws_subnet.public-subnet-a.id
-  route_table_id = aws_route_table.public.id
-}
-
-### Default Security Group ###
-resource "aws_security_group_rule" "sgrprule-ec2" {
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "TCP"
-  description = "sample web page"
-  cidr_blocks = [ "0.0.0.0/0" ]
-  security_group_id = aws_security_group.defaultsgrp.id
-  lifecycle {
-    create_before_destroy = true
+    Name = "public_rt"
   }
 }
 
-resource "aws_security_group" "defaultsgrp" {
+resource "aws_route_table_association" "public_rt_asso" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_instance" "web" {
+  ami             = "ami-005e54dee72cc1d00" 
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.sg.id]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  echo "*** Installing apache2"
+  sudo apt update -y
+  sudo apt install apache2 -y
+  echo "*** Completed Installing apache2"
+  EOF
+
+  tags = {
+    Name = "web_instance"
+  }
+
+  volume_tags = {
+    Name = "web_instance"
+  } 
+}
+
+variable "region" {
+default = "ap-northeast-2"
+}
+variable "instance_type" {}
+variable "public_subnet_cidr" {}
+
+resource "aws_security_group" "sg" {
   name        = "allow_ssh_http"
   description = "Allow ssh http inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.app_vpc.id
 
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   ingress {
     description      = "HTTP from VPC"
@@ -74,6 +92,7 @@ resource "aws_security_group" "defaultsgrp" {
     to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
@@ -81,6 +100,7 @@ resource "aws_security_group" "defaultsgrp" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
@@ -88,43 +108,6 @@ resource "aws_security_group" "defaultsgrp" {
   }
 }
 
-data "aws_ami" "amazon_ami" {
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.0.20220606.1-x86_64-gp2"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  most_recent = true
-  owners      = ["amazon"]
-}
-
-### EC2 ###
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.amazon_ami.id
-  instance_type = "t2.micro"
-  associate_public_ip_address = "true"
-  vpc_security_group_ids = [
-    aws_security_group.defaultsgrp.id
-  ]
-  user_data = <<EOF
-sudo su
-yum update -y
-yum install -y httpd.x86_64
-yum install -y jq
-REGION_AV_ZONE=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .availabilityZone -r`
-systemctl start httpd.service
-systemctl enable httpd.service
-echo “Hello World from $(hostname -f) from the availability zone: $REGION_AV_ZONE” > /var/www/html/index.html
-EOF
-  subnet_id = aws_subnet.public-subnet-a.id
-  tags = {
-    Name = "ec2-${var.service}"
-  }
-}
-
-output "instance_public_ip" {
-  value = aws_instance.web.public_ip   ### Apply가 완료되면 EC2의 Public IP를 반환한다.
+output "web_instance_ip" {
+    value = aws_instance.web.public_ip
 }
