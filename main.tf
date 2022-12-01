d3f09e2a8dce02fcd7e655b8b92312ecba8a047c
hodong <hodong42@gmail.com>
2022-12-01 11:12:18 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,7 +73,6 @@ resource "aws_security_group" "defaultsgrp" {
     to_port          = 443
     protocol         = "tcp"
     cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
 
   egress {
@@ -81,7 +80,6 @@ resource "aws_security_group" "defaultsgrp" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
