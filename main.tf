f9084c6771ccb79ec9fd73cc51a294e551b5c541
hodong <hodong42@gmail.com>
2022-12-01 12:35:35 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -105,10 +105,17 @@ resource "aws_instance" "web" {
   ]
   user_data = <<EOF
 #!/bin/bash
-echo "*** Installing apache2"
-sudo apt update -y
-sudo apt install apache2 -y
-echo "*** Completed Installing apache2"
+yum update -y
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+yum install -y httpd mariadb-server
+systemctl start httpd
+systemctl enable httpd
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
 EOF
   subnet_id = aws_subnet.public-subnet-a.id
   tags = {
