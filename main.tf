d83c1ee8a20a32cb11dde048506a385694666555
hodong <hodong42@gmail.com>
2022-12-01 11:27:52 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -97,7 +97,17 @@ resource "aws_instance" "web" {
   ]
   user_data = <<EOF
 #!/bin/bash
-service httpd start
+yum update -y
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+yum install -y httpd mariadb-server
+systemctl start httpd
+systemctl enable httpd
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
 EOF
   subnet_id = aws_subnet.public-subnet-a.id
   tags = {
